69837cbed7f9345dde09628df785ff9e5a02b49f
djbheron100 <Officialbheron@outlook.com>
2024-10-31 22:19:18 +0200

    Update chanel.js

--- commandes/chanel.js
+++ commandes/chanel.js
@@ -4,8 +4,8 @@ const { zokou } = require("../framework/zokou");
 
 zokou({ nomCom: "channel", reaction: "😌", nomFichier: __filename }, async (dest, zk, commandeOptions) => {
     console.log("Commande saisie !!!s");
-    let z = 'Salut je m\'appelle *YESSER 😎_MD* \n\n ' + 'je suis un bot Whatsapp Multi-appareil voici la chaîne';
-    let d = ' developpé par *Yesser Tech🏹*';
+    let z = 'Salut je m\'appelle *BHERON 😎_MD* \n\n ' + 'je suis un bot Whatsapp Multi-appareil voici la chaîne';
+    let d = ' developpé par *Official Bheron🏹*';
     let varmess = z + d;
     var lien = 'https://whatsapp.com/channel/0029VakA1mu35fM18opH1s30';  // Remplacez cet URL par le lien que vous souhaitez envoyer
     await zk.sendMessage(dest, { text: varmess + "\n" + lien });
