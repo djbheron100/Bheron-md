19ce7b75502a61633fdc59747bbe70a654284378
djbheron100 <Officialbheron@outlook.com>
2024-10-31 22:39:59 +0200

    Update uptime.js

--- commandes/uptime.js
+++ commandes/uptime.js
@@ -28,7 +28,7 @@ zokou({ nomCom: 'uptime',
   async (dest, zk, commandeOptions) => {
     const { ms, arg, repondre } = commandeOptions;
 
-                 await repondre(`*_uptime of YESSER md  bot is: ${runtime(process.uptime())}_*`) 
+                 await repondre(`*_uptime of 𝙱𝙷𝙴𝚁𝙾𝙽 𝙼𝙳  bot is: ${runtime(process.uptime())}_*`) 
 
    
 
