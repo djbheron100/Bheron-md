083fda5fc987f362ba611991bff6a4a1cc2aa42f
djbheron100 <Officialbheron@outlook.com>
2024-10-31 17:47:25 +0200

    Update GPT.js

--- commandes/GPT.js
+++ commandes/GPT.js
@@ -4,11 +4,11 @@ const s = require('../set');
 const axios = require('axios');
 
 /* 
-Created By yesser tech
+Created By Official Bheron
 Don't claim, okey 
 */
 
-zokou({nomCom:"yesser1",reaction:"📡",categorie:"IA"},async(dest,zk,commandeOptions)=>{
+zokou({nomCom:"bheron",reaction:"📡",categorie:"IA"},async(dest,zk,commandeOptions)=>{
 
   const {repondre,ms,arg}=commandeOptions;
   
