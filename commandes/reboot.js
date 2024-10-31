44d5fccc5a22d8c562a917a4d93ec0ea8a198deb
djbheron100 <Officialbheron@outlook.com>
2024-10-31 23:54:37 +0200

    Update reboot.js

--- commandes/reboot.js
+++ commandes/reboot.js
@@ -14,12 +14,12 @@ const{repondre,ms,dev,superUser}=com;
 
   if(!superUser)
   {
-    return repondre("This command is for owner or YESSER TEACH");
+    return repondre("This command is for owner or OFFICIAL BHERON");
   }
 
   const {exec}=require("child_process")
 
-    repondre("YESSER-MD  bot Restarting ⏳");
+    repondre("BHERON-MD  bot Restarting ⏳");
 
   exec("pm2 restart all");
   
