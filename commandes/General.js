c34b309d16b21f747d7edfde958124957cc8ee81
djbheron100 <Officialbheron@outlook.com>
2024-11-02 20:25:46 +0200

    Update General.js

--- commandes/General.js
+++ commandes/General.js
@@ -108,7 +108,7 @@ zokou({ nomCom: "developer", categorie: "General", reaction: "🤠" }, async (de
       // Ajoute d'autres développeurs ici avec leur nom et numéro
     ];
 
-    let message = "👋 *Welcome to Bheron MD* here is the developer numbers:\n\n";
+    let message = "👋 *Welcome to BHERON-MD* here is the developer numbers:\n\n";
     for (const dev of devs) {
       message += `----------------\n• ${dev.nom} : https://wa.me/${dev.numero}\n`;
     }
